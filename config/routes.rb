3bdf05daa64a6e693d8f3d5080216135f6653a95
acdavi <davimilt@gmail.com>
2018-09-24 22:27:18 -0300

    Parte já feita

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#home'
+
+  get 'home', to: 'pages#home'
+
+  get 'help', to: 'pages#ajuda'
+
+  get 'ranking', to: 'pages#rank'
+
+  get 'config', to: 'pages#config'
+end
